149367f9a3a22b9d33e522e337fa87d19804f3a3
naveed333 <m.naveedashfaq@gmail.com>
2021-10-13 11:59:05 +0500

    new contracts 13-10-2021

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,18 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "./Owned.sol";
+import "./Logger.sol";
+import "./IFaucet.sol";
 
-contract Faucet {
-    // this is a special function
-    // it's called when you make a tx that doesn't specify
-    // function name to call
+contract Faucet is Owned, Logger, IFaucet {
+    uint256 public numOfFunders;
 
-    // External function are part of the contract interface
-    // which mean they can be called via contracts and other txs
+    mapping(address => bool) private funders;
+    mapping(uint256 => address) private lutFunders;
+
+    modifier limitWithdraw(uint256 withdrawAmount) {
+        require(
+            withdrawAmount <= 100000000000000000,
+            "Cannot withdraw more than 0.1 ether"
+        );
+        _;
+    }
 
     receive() external payable {}
+
+    function emitLog() public pure override returns (bytes32) {
+        return "Hello World";
+    }
+
+    function addFunds() external payable override {
+        address funder = msg.sender;
+        test3();
+
+        if (!funders[funder]) {
+            uint256 index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
+    }
+
+    function test1() external onlyOwner {
+        // some managing stuff that only admin should have access to
+    }
+
+    function test2() external onlyOwner {
+        // some managing stuff that only admin should have access to
+    }
+
+    function withdraw(uint256 withdrawAmount)
+        external
+        override
+        limitWithdraw(withdrawAmount)
+    {
+        payable(msg.sender).transfer(withdrawAmount);
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for (uint256 i = 0; i < numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        return lutFunders[index];
+    }
 }
 
-// block info
-// Nounce - a hash that when combined with the minHash proofs that
-//  the block has gone through proof of work(POW)
-// 8 bytes => 64 bits
+// const instance = await Faucet.deployed();
+
+// instance.addFunds({from: accounts[0], value: "2000000000000000000"})
+// instance.addFunds({from: accounts[1], value: "2000000000000000000"})
+
+// instance.withdraw("500000000000000000", {from: accounts[1]})
+
+// instance.getFunderAtIndex(0)
+// instance.getAllFunders()
