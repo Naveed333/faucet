e499adc4ba562309e10485854da19c422eb6a6e0
naveed333 <m.naveedashfaq@gmail.com>
2021-10-13 13:38:56 +0500

    integrate web3 in app 13-10-2021

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import web3 from 'web3';
+import Web3 from 'web3';
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable();
+        } catch (error) {
+          console.error('User denied account access');
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new web3.providers.HttpProvider('http:localhost:7475');
+      }
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      });
+    };
+    loadProvider();
+  }, []);
+  console.log(web3Api.web3);
   return (
     <>
       <div className='faucet-wrapper'>
